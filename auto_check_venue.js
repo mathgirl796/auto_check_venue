e79ebd9a9e2c2c3a5b8ae089582362ed5134cb3f
mathgirl796 <aduan456@126.com>
2022-10-16 16:05:23 +0800

    update ring() and refresh(), improve its stability

--- auto_check_venue.js
+++ auto_check_venue.js
@@ -1,4 +1,4 @@
-function ring_once() {
+function ring_once(freq) {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var audioCtx = new AudioContext();
     var oscillator = audioCtx.createOscillator();
@@ -6,7 +6,7 @@ function ring_once() {
     oscillator.connect(gainNode);
     gainNode.connect(audioCtx.destination);
     oscillator.type = "sine";
-    oscillator.frequency.value = 196.00;
+    oscillator.frequency.value = freq;
     gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
     gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.01);
     oscillator.start(audioCtx.currentTime);
@@ -15,6 +15,15 @@ function ring_once() {
     delete audioCtx;
     delete oscillator;
     delete gainNode;
+}
+
+function ring(times) {
+    var latency = 200;
+    var sum = 0;
+    for (var i=0;i<times;i++) {
+        setTimeout(ring_once, sum, 261.626);
+        sum += latency;
+    }
 }
 
 function check_once() {
@@ -28,7 +37,7 @@ function check_once() {
         if (s[0] == "1" && s.indexOf("点击") >= 0) {
             msg = place + " " + date + " " + s.split("\n")[0] + " " + "可以预约";
             console.log(msg);
-            ring_once();
+            ring(5);
         }
     }
     button = document.querySelector("#app > div > div.card > div:nth-child(4) > div > div > div.session-head > div.time-select > div.time-line > div.time-sides");
@@ -42,8 +51,9 @@ function check_one_place() {
 
 function refresh() {
     console.log("刷新页面");
-    document.close();
-    document.write('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,user-scalable=no,minimum-scale=1,maximum-scale=1"><title>工大app</title><link rel="stylesheet" href="./css/animate.min.css"><link href="/css/chunk-20e26175.67a22edd.css" rel="prefetch"><link href="/css/chunk-4416c270.6c7b48ac.css" rel="prefetch"><link href="/css/chunk-5a9a4e8a.7c3ccc14.css" rel="prefetch"><link href="/css/chunk-647f7d60.684fb414.css" rel="prefetch"><link href="/css/chunk-77da2deb.9bab7b05.css" rel="prefetch"><link href="/css/chunk-7eb5f846.f279205d.css" rel="prefetch"><link href="/css/chunk-940ee052.3fe749b1.css" rel="prefetch"><link href="/css/chunk-9707ac80.b581d151.css" rel="prefetch"><link href="/css/chunk-adf0aafa.a9d0276b.css" rel="prefetch"><link href="/css/chunk-c92ec57e.dc7a3c67.css" rel="prefetch"><link href="/css/chunk-fee98ec0.e8b536de.css" rel="prefetch"><link href="/js/chunk-20e26175.96e7fa6f.js" rel="prefetch"><link href="/js/chunk-4416c270.1bf09d3c.js" rel="prefetch"><link href="/js/chunk-502fbe04.3caa6a4b.js" rel="prefetch"><link href="/js/chunk-5a9a4e8a.88bbf547.js" rel="prefetch"><link href="/js/chunk-647f7d60.c3262bac.js" rel="prefetch"><link href="/js/chunk-77da2deb.4651e602.js" rel="prefetch"><link href="/js/chunk-7eb5f846.f9303ac6.js" rel="prefetch"><link href="/js/chunk-940ee052.0fd430e3.js" rel="prefetch"><link href="/js/chunk-9707ac80.730944a5.js" rel="prefetch"><link href="/js/chunk-adf0aafa.26e741aa.js" rel="prefetch"><link href="/js/chunk-c92ec57e.36c618f6.js" rel="prefetch"><link href="/js/chunk-fee98ec0.d744fbfc.js" rel="prefetch"><link href="/css/app.df549113.css" rel="preload" as="style"><link href="/css/chunk-vendors.d0cb2b5c.css" rel="preload" as="style"><link href="/js/app.0b35a9bd.js" rel="preload" as="script"><link href="/js/chunk-vendors.b6e57c9d.js" rel="preload" as="script"><link href="/css/chunk-vendors.d0cb2b5c.css" rel="stylesheet"><link href="/css/app.df549113.css" rel="stylesheet"></head><body><div id="app"></div><script src="/js/chunk-vendors.b6e57c9d.js"></script><script src="/js/app.0b35a9bd.js"></script></body></html>');
+    document.querySelector("#app > div.page-container > div.sport-nav > div.item-container > div > div:nth-child(1) > img").click();
+    // document.close();
+    // document.write('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,user-scalable=no,minimum-scale=1,maximum-scale=1"><title>工大app</title><link rel="stylesheet" href="./css/animate.min.css"><link href="/css/chunk-20e26175.67a22edd.css" rel="prefetch"><link href="/css/chunk-4416c270.6c7b48ac.css" rel="prefetch"><link href="/css/chunk-5a9a4e8a.7c3ccc14.css" rel="prefetch"><link href="/css/chunk-647f7d60.684fb414.css" rel="prefetch"><link href="/css/chunk-77da2deb.9bab7b05.css" rel="prefetch"><link href="/css/chunk-7eb5f846.f279205d.css" rel="prefetch"><link href="/css/chunk-940ee052.3fe749b1.css" rel="prefetch"><link href="/css/chunk-9707ac80.b581d151.css" rel="prefetch"><link href="/css/chunk-adf0aafa.a9d0276b.css" rel="prefetch"><link href="/css/chunk-c92ec57e.dc7a3c67.css" rel="prefetch"><link href="/css/chunk-fee98ec0.e8b536de.css" rel="prefetch"><link href="/js/chunk-20e26175.96e7fa6f.js" rel="prefetch"><link href="/js/chunk-4416c270.1bf09d3c.js" rel="prefetch"><link href="/js/chunk-502fbe04.3caa6a4b.js" rel="prefetch"><link href="/js/chunk-5a9a4e8a.88bbf547.js" rel="prefetch"><link href="/js/chunk-647f7d60.c3262bac.js" rel="prefetch"><link href="/js/chunk-77da2deb.4651e602.js" rel="prefetch"><link href="/js/chunk-7eb5f846.f9303ac6.js" rel="prefetch"><link href="/js/chunk-940ee052.0fd430e3.js" rel="prefetch"><link href="/js/chunk-9707ac80.730944a5.js" rel="prefetch"><link href="/js/chunk-adf0aafa.26e741aa.js" rel="prefetch"><link href="/js/chunk-c92ec57e.36c618f6.js" rel="prefetch"><link href="/js/chunk-fee98ec0.d744fbfc.js" rel="prefetch"><link href="/css/app.df549113.css" rel="preload" as="style"><link href="/css/chunk-vendors.d0cb2b5c.css" rel="preload" as="style"><link href="/js/app.0b35a9bd.js" rel="preload" as="script"><link href="/js/chunk-vendors.b6e57c9d.js" rel="preload" as="script"><link href="/css/chunk-vendors.d0cb2b5c.css" rel="stylesheet"><link href="/css/app.df549113.css" rel="stylesheet"></head><body><div id="app"></div><script src="/js/chunk-vendors.b6e57c9d.js"></script><script src="/js/app.0b35a9bd.js"></script></body></html>');
 }
 
 function todo_list() {
